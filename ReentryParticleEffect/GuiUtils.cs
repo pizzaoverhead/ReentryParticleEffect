72af9d7a035cda974f52b1db1c0bed60c7788415
pizzaoverhead <pizzaoverhead@gmail.com>
2018-08-24 20:10:06 +0100

    Recompiled and updated for KSP 1.4 compatibility. Testing plasma coloured by temperature.

---
+++ ReentryParticleEffect/GuiUtils.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.Text.RegularExpressions;
+
+namespace ReentryParticleEffect
+{
+    public static class GuiUtils
+    {
+        public static void label(string text, object obj)
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.Label(text);
+            GUILayout.FlexibleSpace();
+            GUILayout.Label(obj == null ? "null" : obj.ToString(), GUILayout.Width(100));
+            GUILayout.EndHorizontal();
+        }
+
+        /*public static float editFloat(string label, float value)
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.Label(label);
+            GUILayout.FlexibleSpace();
+            bool maxVal = value == float.MaxValue;
+            bool minVal = value == float.MinValue;
+            string displayVal = maxVal || minVal ? "" : value.ToString();
+            string v = GUILayout.TextField(displayVal, GUILayout.Width(200));
+            GUILayout.EndHorizontal();
+            float f = value;
+            if (v == string.Empty)
+            {
+                if (maxVal)
+                    f = float.MaxValue;
+                else if (minVal)
+                    f = float.MinValue;
+                else
+                    f = 0;
+            }
+            else
+                float.TryParse(v, out f);
+            return f;
+        }*/
+
+        public static string editFloat(string label, string text, out float value, float defaultValue)
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.Label(label);
+            GUILayout.FlexibleSpace();
+            string newText = GUILayout.TextField(text, GUILayout.Width(200));
+            GUILayout.EndHorizontal();
+            if (String.IsNullOrEmpty(text))
+                value = defaultValue;
+            else
+            {
+                Regex numericOnly = new Regex(@"[^\d.-]");
+                newText = numericOnly.Replace(newText, "");
+
+                float.TryParse(newText, out value);
+            }
+            //if (text != newText)
+            //{
+            //Debug.Log("####Updated " + label + " to " + value);
+            //}
+            return newText;
+        }
+
+        public static string editString(string label, string value)
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.Label(label);
+            GUILayout.FlexibleSpace();
+            string v = GUILayout.TextField(value, GUILayout.Width(200));
+            GUILayout.EndHorizontal();
+            return v;
+        }
+
+        public static T editEnum<T>(string label, T value) where T : struct, IConvertible
+        {
+            Type genericType = typeof(T);
+            if (!genericType.IsEnum)
+                throw new ArgumentException("Type 'T' must be an enum");
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Label(label);
+            //GUILayout.FlexibleSpace();
+            GUILayout.Label(" ");
+            foreach (var e in Enum.GetValues(typeof(T)))
+            {
+                string name = e.ToString();
+                GUILayout.Toggle(false, ""); // Text parameter causes overlap.
+                GUILayout.Label(name + " ");
+            }
+            GUILayout.EndHorizontal();
+
+            return value;
+        }
+
+        public static void slider(string label, ref float variable, float from, float to)
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.Label(label + ": " + variable.ToString());
+            GUILayout.FlexibleSpace();
+            variable = GUILayout.HorizontalSlider(variable, from, to, GUILayout.Width(100));
+            GUILayout.EndHorizontal();
+        }
+
+        public static Color rgbaSlider(string label, ref float r, ref float g, ref float b, ref float a, float from, float to)
+        {
+            GUILayout.Label(label);
+            slider("r", ref r, from, to);
+            slider("g", ref g, from, to);
+            slider("b", ref b, from, to);
+            slider("a", ref a, from, to);
+            return new Color(r, g, b, a);
+
+        }
+
+        static float x = 0;
+        static float y = 0;
+        public static KeyValuePair<float, float> GetSliderXY()
+        {
+            return new KeyValuePair<float, float>(x, y);
+        }
+    }
+
+    /*
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class TestGui : MonoBehaviour
+    {
+        private static Rect windowPos = new Rect(Screen.width / 4, Screen.height / 4, 10f, 10f);
+
+        /// <summary>
+        /// GUI draw event. Called (at least once) each frame.
+        /// </summary>
+        public void OnGUI()
+        {
+            if (ReentryParticleEffect.DrawGui)
+                windowPos = GUILayout.Window(GetInstanceID(), windowPos, MainGUI, "Reentry Particle Effect", GUILayout.Width(600), GUILayout.Height(50));
+        }
+        private static void MainGUI(int windowID)
+        {
+            GUILayout.BeginVertical();
+
+            ReentryParticleEffect.Gui();
+
+            GUILayout.EndVertical();
+            GUI.DragWindow();
+        }
+    }*/
+}
--- ReentryParticleEffect/ReentryParticleEffect.cs
+++ ReentryParticleEffect/ReentryParticleEffect.cs
@@ -6,16 +6,26 @@ using UnityEngine;
 
 namespace ReentryParticleEffect
 {
+    /*
+     * BUGS
+     * 
+     * Trail too dense
+     * Trail doesn't ease in at top of atmosphere
+     * Trail isn't deleted on craft destroyed
+     * Trail too short?
+     */
     [KSPAddon(KSPAddon.Startup.Flight, false)]
     public class ReentryParticleEffect : MonoBehaviour
     {
         public Vector3 velocity;
-        public int MaxParticles = 3000;
-        public int MaxEmissionRate = 400;
+        public static int MaxParticles = 3000;
+        public static int MaxEmissionRate = 200;
         // Minimum reentry strength that the effects will activate at.
         // 0 = Activate at the first sign of the flame effects.
         // 1 = Never activate, even at the strongest reentry strength.
-        public float EffectThreshold = 0.5f;
+        public float EffectThreshold = 0.4f;
+
+        public static bool DrawGui = true;
 
         private void Start()
         {
@@ -36,24 +46,32 @@ namespace ReentryParticleEffect
             public ParticleSystem Trail;
             public ParticleSystem Sparks;
 
-            public void Die ()
+            public void Die()
             {
-                Destroy (trailPrefab);
-                Destroy (Trail);
-                Destroy (Sparks);
+                Destroy(trailPrefab);
+                Destroy(Trail);
+                Destroy(Sparks);
             }
         }
 
-        public ReentryEffect GetEffect()
+        public ReentryEffect CreateEffect()
         {
             GameObject effect = (GameObject)GameObject.Instantiate(Resources.Load("Effects/fx_reentryTrail"));
             ReentryEffect reentryFx = new ReentryEffect(effect);
-            reentryFx.Trail.playbackSpeed = 5;
-            reentryFx.Sparks.playbackSpeed = 5;
+            // Set the effect speed high to animate as fast as is visible.
+            var trailMain = reentryFx.Trail.main;
+            trailMain.simulationSpeed = 5;
+            var sparksMain = reentryFx.Sparks.main;
+            sparksMain.simulationSpeed = 5;
+
             return reentryFx;
         }
 
-        public Dictionary<Guid, ReentryEffect> VesselDict = new Dictionary<Guid, ReentryEffect>();
+        public static Dictionary<Guid, ReentryEffect> VesselDict = new Dictionary<Guid, ReentryEffect>();
+
+        // Temporary for GUI
+        /*private static float effectStrength = 0;
+        private static AerodynamicsFX afx1 = null;*/
 
         private void FixedUpdate()
         {
@@ -62,14 +80,14 @@ namespace ReentryParticleEffect
             for (int i = vessels.Count - 1; i >= 0; --i)
             {
                 Vessel vessel = vessels[i];
-                ReentryEffect effects  = null;
+                ReentryEffect effects = null;
                 if (VesselDict.ContainsKey(vessel.id))
                     effects = VesselDict[vessel.id];
                 else
                 {
                     if (vessel.loaded)
                     {
-                        effects = GetEffect();
+                        effects = CreateEffect();
                         VesselDict.Add(vessel.id, effects);
                     }
                     else
@@ -80,7 +98,7 @@ namespace ReentryParticleEffect
                 {
                     if (effects != null)
                     {
-                        effects.Die ();
+                        effects.Die();
                     }
                     effects = null;
                     continue;
@@ -89,41 +107,46 @@ namespace ReentryParticleEffect
                 if (effects == null || effects.Trail == null || effects.Sparks == null)
                     continue;
 
+                ParticleSystem.EmissionModule trailEmission = effects.Trail.emission;
+                ParticleSystem.EmissionModule sparksEmission = effects.Sparks.emission;
                 if (AeroFX != null)
                 {
+                    //afx1 = AeroFX;
                     // FxScalar: Strength of the effects.
                     // state: 0 = condensation, 1 = reentry.
                     if (effectStrength > 0)
                     {
                         // Ensure the particles don't lag a frame behind.
                         effects.Trail.transform.position = vessel.CoM + vessel.rb_velocity * Time.fixedDeltaTime;
-                        effects.Trail.enableEmission = true;
+                        trailEmission.enabled = true;
                         effects.Sparks.transform.position = vessel.CoM + vessel.rb_velocity * Time.fixedDeltaTime;
-                        effects.Sparks.enableEmission = true;
+                        sparksEmission.enabled = true;
 
                         velocity = AeroFX.velocity * (float)AeroFX.airSpeed;
 
-                        effects.Trail.startSpeed = velocity.magnitude;
+                        var trailMain = effects.Trail.main;
+                        trailMain.startSpeed = velocity.magnitude;
                         effects.Trail.transform.forward = -velocity.normalized;
-                        effects.Trail.maxParticles = (int)(MaxParticles * effectStrength);
-                        effects.Trail.emissionRate = (int)(MaxEmissionRate * effectStrength);
+                        trailMain.maxParticles = (int)(MaxParticles * effectStrength);
+                        trailEmission.rateOverTime = (int)(MaxEmissionRate * effectStrength);
 
                         // startSpeed controls the emission cone angle. Greater than ~1 is too wide.
                         //reentryTrailSparks.startSpeed = velocity.magnitude;
+                        var sparksMain = effects.Sparks.main;
                         effects.Sparks.transform.forward = -velocity.normalized;
-                        effects.Sparks.maxParticles = (int)(MaxParticles * effectStrength);
-                        effects.Sparks.emissionRate = (int)(MaxEmissionRate * effectStrength);
+                        sparksMain.maxParticles = (int)(MaxParticles * effectStrength);
+                        sparksEmission.rateOverTime = (int)(MaxEmissionRate * effectStrength);
                     }
                     else
                     {
-                        effects.Trail.enableEmission = false;
-                        effects.Sparks.enableEmission = false;
+                        trailEmission.enabled = false;
+                        sparksEmission.enabled = false;
                     }
                 }
                 else
                 {
-                    effects.Trail.enableEmission = false;
-                    effects.Sparks.enableEmission = false;
+                    trailEmission.enabled = false;
+                    sparksEmission.enabled = false;
                 }
             }
         }
@@ -135,7 +158,7 @@ namespace ReentryParticleEffect
                 ReentryEffect effects = VesselDict[vessel.id];
                 if (effects != null)
                 {
-                    effects.Die ();
+                    effects.Die();
                 }
                 VesselDict.Remove(vessel.id);
             }
@@ -155,5 +178,198 @@ namespace ReentryParticleEffect
                 return _aeroFX;
             }
         }
+        /*
+        public static Color BlackBodyToRgb(float tempKelvin)
+        {
+            // C# implementation of Tanner Helland's approximation from here:
+            // http://www.tannerhelland.com/4435/convert-temperature-rgb-algorithm-code/
+            // For use with temperatures between 1000 and 40,000 K.
+            float temp = tempKelvin / 100;
+            // Colour values (0 to 255).
+            float red = 0;
+            float green = 0;
+            float blue = 0;
+
+            // Calculate red
+            if (temp <= 66)
+            {
+                red = 255;
+            }
+            else
+            {
+                red = temp - 60f;
+                red = 329.698727446f * (float)(Math.Pow(red, -0.1332047592f));
+                if (red < 0)
+                    red = 0;
+                if (red > 255)
+                    red = 255;
+            }
+
+            // Calculate green
+            if (temp <= 66)
+            {
+                green = temp;
+                green = 99.4708025861f * (float)Math.Log(green) - 161.1195681661f;
+                if (green < 0)
+                    green = 0;
+                if (green > 255)
+                    green = 255;
+            }
+            else
+            {
+                green = temp - 60;
+                green = 288.1221695283f * (float)Math.Pow(green, -0.0755148492f);
+                if (green < 0)
+                    green = 0;
+                if (green > 255)
+                    green = 255;
+            }
+
+            // Calculate Blue
+            if (temp <= 66)
+            {
+                blue = 255;
+            }
+            else
+            {
+                blue = temp - 10;
+                blue = 138.5177312231f * (float)Math.Log(blue) - 305.0447927307f;
+                if (blue < 0)
+                    blue = 0;
+                if (blue > 255)
+                    blue = 255;
+            }
+
+            return new Color(red/255, green/255, blue/255, 1);
+        }
+
+        private static string _trailPlaybackText = "5";
+        private static string _sparksPlaybackText = "5";
+        public static void Gui()
+        {
+            ReentryEffect effects = null;
+            if (VesselDict.ContainsKey(FlightGlobals.ActiveVessel.id))
+                effects = VesselDict[FlightGlobals.ActiveVessel.id];
+
+            if (effects == null)
+            {
+                GUILayout.Label("ReentryFX is null");
+                return;
+            }
+            if (effects.Trail == null)
+            {
+                GUILayout.Label("Trail is null");
+            }
+            if (effects.Sparks == null)
+            {
+                GUILayout.Label("Sparks is null");
+            }
+            
+            GuiUtils.label("Effect Strength", effectStrength);
+            GuiUtils.label("Stock effect Strength", afx1.FxScalar * afx1.state);
+            
+            float highestTemp = GetVesselMaxSkinTemp();
+            Color blackBodyColor = BlackBodyToRgb(highestTemp);
+            if (FlightGlobals.ActiveVessel != null && FlightGlobals.ActiveVessel.rootPart != null)
+            {
+                GuiUtils.label("Highest temp", highestTemp);
+                GuiUtils.label("Blackbody colour", blackBodyColor);
+
+                GuiUtils.label("temperature", FlightGlobals.ActiveVessel.rootPart.temperature);
+                GuiUtils.label("skinTemperature", FlightGlobals.ActiveVessel.rootPart.skinTemperature);
+                GuiUtils.label("skinUnexposedExternalTemp", FlightGlobals.ActiveVessel.rootPart.skinUnexposedExternalTemp);
+                GuiUtils.label("tempExplodeChance", FlightGlobals.ActiveVessel.rootPart.tempExplodeChance);
+
+                GuiUtils.label("MaxTemp", FlightGlobals.ActiveVessel.rootPart.maxTemp);
+                GuiUtils.label("SkinMaxTemp", FlightGlobals.ActiveVessel.rootPart.skinMaxTemp);
+            }
+
+            GUILayout.Label("Max Particles");
+            MaxParticles = (int)GUILayout.HorizontalSlider(MaxParticles, 0, 10000);
+            GUILayout.Label("Max Emission Rate");
+            MaxEmissionRate = (int)GUILayout.HorizontalSlider(MaxEmissionRate, 0, 1000);
+
+            GUILayout.Label("Trail");
+            if (effects.Trail == null)
+                GUILayout.Label("Trail is null");
+            else
+            {
+                var trailMain = effects.Trail.main;
+                float trailPlaybackSpeed = trailMain.simulationSpeed;
+                _trailPlaybackText = GuiUtils.editFloat("Playback speed", _trailPlaybackText, out trailPlaybackSpeed, 5);
+                trailMain.simulationSpeed = trailPlaybackSpeed;
+
+                //Color key0 = new Color(1f, 0.545f, 0.192f, 1f);
+                //Color key1 = new Color(0.725f, 0.169f, 0f, 1f);
+                Color key0 = effects.Trail.colorOverLifetime.color.gradient.colorKeys[0].color;
+                Color key1 = effects.Trail.colorOverLifetime.color.gradient.colorKeys[1].color;
+
+                /*Gradient grad = new Gradient();
+                grad.SetKeys(
+                    new GradientColorKey[] { new GradientColorKey(Color.blue, 0.0f), new GradientColorKey(Color.red, 1.0f) }, 
+                    new GradientAlphaKey[] { new GradientAlphaKey(1.0f, 0.0f), new GradientAlphaKey(0.0f, 1.0f) }
+                    );
+                * /
+
+                key0 = GuiUtils.rgbaSlider("Colour Gradient 0", ref key0.r, ref key0.g, ref key0.b, ref key0.a, 0f, 1f);
+                key1 = GuiUtils.rgbaSlider("Colour Gradient 1", ref key1.r, ref key1.g, ref key1.b, ref key1.a, 0f, 1f);
+
+                effects.Trail.colorOverLifetime.color.gradient.colorKeys[0].color = key0;
+                effects.Trail.colorOverLifetime.color.gradient.colorKeys[1].color = key1;
+
+                //Color cMax = trailMain.startColor.colorMax;
+                //Color cMin = trailMain.startColor.colorMin;
+                //cMax = GuiUtils.rgbaSlider("Max Colour", ref cMax.r, ref cMax.g, ref cMax.b, ref cMax.a, 0f, 1f);
+                //cMin = GuiUtils.rgbaSlider("Min Colour", ref cMin.r, ref cMin.g, ref cMin.b, ref cMin.a, 0f, 1f);
+                //trailMain.startColor = new ParticleSystem.MinMaxGradient(cMin, cMax);
+                //trailMain.startColor = new ParticleSystem.MinMaxGradient(blackBodyColor, BlackBodyToRgb(highestTemp * 2));
+            }
+
+            /*
+            GUILayout.Label("Sparks");
+            if (effects.Sparks == null)
+                GUILayout.Label("Sparks is null");
+            else
+            {
+                var sparksMain = effects.Sparks.main;
+                float sparksPlaybackSpeed = sparksMain.simulationSpeed;
+                _sparksPlaybackText = GuiUtils.editFloat("Playback speed", _sparksPlaybackText, out sparksPlaybackSpeed, 5);
+                sparksMain.simulationSpeed = sparksPlaybackSpeed;
+            }* /
+        }
+
+        public static float GetVesselMaxSkinTemp()
+        {
+            float maxTemp = 0;
+
+            int partCount = FlightGlobals.ActiveVessel.parts.Count;
+            for (int i = 0; i < partCount; i++)
+            {
+                maxTemp = (float)Math.Max(FlightGlobals.ActiveVessel.parts[i].skinTemperature, maxTemp);
+            }
+
+            return maxTemp;
+        }*/
     }
+
+    /*[KSPAddon(KSPAddon.Startup.MainMenu, false)]
+    class AutoStartup : UnityEngine.MonoBehaviour
+    {
+        public static bool first = true;
+        public void Start()
+        {
+            //only do it on the first entry to the menu
+            if (first)
+            {
+                first = false;
+                HighLogic.SaveFolder = "test";
+                /*var game = GamePersistence.LoadGame("persistent", HighLogic.SaveFolder, true, false);
+                if (game != null && game.flightState != null && game.compatible)
+                    FlightDriver.StartAndFocusVessel(game, game.flightState.activeVesselIdx);* /
+                CheatOptions.InfinitePropellant = true;
+                CheatOptions.InfiniteElectricity = true;
+                CheatOptions.IgnoreMaxTemperature = true;
+            }
+        }
+    }*/
 }
